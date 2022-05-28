bd14f68ca0903d684fa881d19450865c3534a0e4
GildanFadilah21 <hotelmajapahit21@gmail.com>
2022-05-29 03:00:10 +0700

    majapahit

---
+++ app/Views/Layout/Footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-auto py-3 bg-info" > 
+  <div class="container">
+<span class="text-white">gildan fadilah</span> 
+  </div>
+</footer> 
+<script src="/js/jquery-3.6.0.min.js"></script>
+<script src="/js/bootstrap.min.js"></script> 
+  </body>
+</html> 
+
